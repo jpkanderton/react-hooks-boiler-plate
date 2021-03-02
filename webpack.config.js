6da25a9f70ddb70cdf18d4756e241eba1777b47f
John Anderton <janderton24@gmail.com>
2021-03-02 12:04:21 -0700

    adds to the webpack.config.js to create bundle.js file upon build script running

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  entry: './client/src/index.js',
+  output: {
+    path: path.resolve(__dirname, '/client/dist'),
+    filename: "bundle.js"
+  },
+  watch: true,
+  mode: 'development',
+  module: {
+    rules: [{
+     test: /\.jsx?$/,
+     exclude: /node_modules/,
+     use: {
+       loader: 'babel-loader',
+       options: {
+         presets: ['@babel/preset-env', '@babel/preset-react']
+       }
+     }
+   }]
+  }
+};
