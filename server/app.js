3f72c80d461f595fd3c15967a1ff6a47050b3c80
John Anderton <janderton24@gmail.com>
2021-03-02 11:50:04 -0700

    adds to server/app.js file: body-parser, express, app, serve static file, respond to get requests with hello world, and exports app from file

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+});
+
+module.exports = app;
